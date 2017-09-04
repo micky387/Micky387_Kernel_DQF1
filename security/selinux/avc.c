fdbb79de3f9ddc863692e74bc79a2ff5af5e731c
micky387 <mickaelsaibi@free.fr>
2017-09-04 13:07:32 +0200

    fixup! Selinux: Add permissive value. fix this commit ... https://github.com/micky387/Micky387_Kernel_DQF1/commit/69b51a3b5d613bb3ff59d2665e677fbdda5b9bbc

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -990,7 +990,7 @@ static noinline int avc_denied(u32 ssid, u32 tsid,
 {
 	if (flags & AVC_STRICT)
 		return -EACCES;
-if defined(CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE)
+#if defined(CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE)
 	if (!(avd->flags & AVD_FLAGS_PERMISSIVE))
 #else
 	if (selinux_enforcing && !(avd->flags & AVD_FLAGS_PERMISSIVE))
