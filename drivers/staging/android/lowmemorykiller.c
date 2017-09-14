ee40cf61bf3924a7ead36f3d1abf0e5758c2460c
micky387 <mickaelsaibi@free.fr>
2017-09-14 13:19:27 +0200

    lowmemorykiller.c: remove samsung stuff with old zsmalloc zram

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -147,10 +147,10 @@ static DEFINE_MUTEX(scan_mutex);
 #define CMA_PAGE_RATIO		70
 #endif
 
-#if defined(CONFIG_ZSWAP)
+/*#if defined(CONFIG_ZSWAP)
 extern atomic_t zswap_pool_pages;
 extern atomic_t zswap_stored_pages;
-#endif
+#endif*/
 
 static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 {
@@ -279,14 +279,14 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			continue;
 		}
 		tasksize = get_mm_rss(p->mm);
-#if defined(CONFIG_ZSWAP)
+/*#if defined(CONFIG_ZSWAP)
 		if (atomic_read(&zswap_stored_pages)) {
 			lowmem_print(3, "shown tasksize : %d\n", tasksize);
 			tasksize += atomic_read(&zswap_pool_pages) * get_mm_counter(p->mm, MM_SWAPENTS)
 				/ atomic_read(&zswap_stored_pages);
 			lowmem_print(3, "real tasksize : %d\n", tasksize);
 		}
-#endif
+#endif*/
 
 #ifdef CONFIG_SAMP_HOTNESS
 		hotness_adj = p->signal->hotness_adj;
